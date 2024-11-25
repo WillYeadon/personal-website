e31d618671e7044844a6800eb3076eb4c6cfd61a
WillYeadon <will.yeadon@durham.ac.uk>
2024-11-25 22:22:56 +0000

    ignore

--- index-ws.js
+++ index-ws.js
@@ -8,3 +8,31 @@ app.get('/', function(req, res) {
 
 server.on('request', app);
 server.listen(3000, function() { console.log('Listening on 3000');});
+
+/** Begin websocket */
+const websocketServer = require('ws').Server;
+
+const wss = new websocketServer({server: server});
+
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size;
+    console.log('Clients connected', numClients);
+
+    wss.broadcast(`Current visitors: ${numClients}`);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome to my server');
+    }
+
+    ws.on('close', function close() {
+        wss.broadcast(`Current visitors: ${numClients}`);
+        console.log('A client has disconnected');
+    });
+
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+}
