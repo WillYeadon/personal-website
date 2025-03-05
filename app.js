4e547ac9b838693239e0a105d6d360a0ef219f55
WillYeadon <will.yeadon@durham.ac.uk>
2025-03-05 22:07:11 +0000

    handle port

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require('http');
 
-http.createServer(function (req, res) {
-res.write("Will Yeadon's website.");
-res.end();
+const server = http.createServer((req, res) => {
+    res.write("Will Yeadon's website.");
+    res.end();
+});
 
-
-}
-).listen(3000);
-
-console.log("Server started on port 3000");
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+}).on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.error(`Port ${PORT} is already in use. Retrying in 5 seconds...`);
+        setTimeout(() => {
+            server.close();
+            server.listen(PORT);
+        }, 5000);
+    } else {
+        console.error("Unexpected error:", err);
+        process.exit(1);
+    }
+});
